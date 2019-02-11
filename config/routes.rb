2a7fc184d21f97865d914774486d01e043a2d706
kitano <ktnydi@yahoo.co.jp>
2019-02-12 07:02:55 +0900

    changed how to write route of posts to resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  get 'posts/new' => 'posts#new'
-  post 'posts' => 'posts#create'
-  delete 'posts/:id' => 'posts#destroy'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
