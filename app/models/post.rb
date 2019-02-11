ecec34bee0fc350d75ef0802f6bf6ae43531f43e
kitano <ktnydi@yahoo.co.jp>
2019-02-12 00:38:58 +0900

    added style home page and responsive css.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true,
+                    length: { maximum: 50 }
   validates :content, presence: true
-  
+
 end
