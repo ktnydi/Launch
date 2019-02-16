cfa041ffadc3baf5b3c2971744142a95b0f533db
kitano <ktnydi@yahoo.co.jp>
2019-02-17 08:25:13 +0900

    fixed edit user account
    user account can be changed excerpt password without current password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
-  
+  attr_accessor :current_password
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
