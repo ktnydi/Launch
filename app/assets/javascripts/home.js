af7abf383b2ce2df3a5f532479994f737dcab6ba
kitano <ktnydi@yahoo.co.jp>
2019-02-19 13:58:45 +0900

    added pv count for Posts
    add access ranking:
    - today
    - this week
    - this month

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var periods = document.querySelectorAll('.select-period a')
+  var now_url = location.href;
+  for (var i = 0; i < periods.length; i++) {
+    var period = periods.item(i)
+    if (period.href === now_url) {
+      period.className = 'select';
+      break;
+    }
+  }
+})
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TestMd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = "Tokyo"
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       %Q(<span class="field_with_errors">#{html_tag}</span>).html_safe
     end
