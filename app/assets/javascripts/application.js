9f2ca2dde44585a284c5192ac644ad194bd80195
kitano <ktnydi@yahoo.co.jp>
2019-02-22 13:43:24 +0900

    fixed application.js
    reason is
    - In posts edit page, code style don't arrange.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,19 +52,4 @@ document.addEventListener('DOMContentLoaded', function() {
       3000
     );
   }
-
-  hljs.initHighlightingOnLoad();
-  marked_js_render();
 });
-
-function marked_js_render() {
-  var renderer = new marked.Renderer();
-
-  renderer.code = function (code, language) {
-    return '<pre' + '><code class="hljs">' + hljs.highlightAuto(code, [language]).value + '</code></pre>';
-  };
-  marked.setOptions({
-    breaks: true,
-    renderer: renderer
-  });
-}
