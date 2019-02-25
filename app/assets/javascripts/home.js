05cecfc5c4f74230b3a5e739ae19320e03756e7f
kitano <ktnydi@yahoo.co.jp>
2019-02-25 11:26:55 +0900

    fixed style in home

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var periods = document.querySelectorAll('.select-period a')
+  var periods = document.querySelectorAll('.select-period a');
   var now_url = location.href;
   for (var i = 0; i < periods.length; i++) {
     var period = periods.item(i)
