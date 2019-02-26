186feab63506fb1a097a206eddbd02da18ce0934
kitano <ktnydi@yahoo.co.jp>
2019-02-26 17:22:38 +0900

    added i18n(:ja)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def update
+    if params[:user][:image_name]
+      image = params[:user][:image_name]
+      image_name = "#{current_user.id}.png"
+      image_path = Rails.root.join("public/images/users/#{image_name}").to_s
+      File.binwrite(image_path, image.read)
+      params[:user][:image_name] = image_name
+    end
+    super
+  end
+
   protected
 
   def update_resource(resource, params)
-    if params[:password].present? && params[:password_confirmation].present?
-      resource.update_attributes(params)
+    if commit_message == "パスワードを変更"
+      resource.update_with_password(params)
     else
       resource.update_without_password(params)
     end
+  end
+
+  def commit_message
+    params[:commit]
   end
 end
