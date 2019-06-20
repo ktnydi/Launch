727325abc83b7df882e5b86130952147584afe47
kitano <ktnydi@yahoo.co.jp>
2019-06-20 21:18:54 +0900

    [add] generators to application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Launch
       %Q(#{html_tag}).html_safe
     end
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+      g.skip_routes true
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
