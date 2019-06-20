5d94c54eac773a418bdee7cf253da36aefa22b92
kitano <ktnydi@yahoo.co.jp>
2019-06-20 21:14:21 +0900

    [delete] drop_table posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_081020) do
+ActiveRecord::Schema.define(version: 2019_06_20_114622) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_id", null: false
@@ -32,17 +32,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_081020) do
     t.string "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "app_url"
-    t.string "user_id", default: "", null: false
-    t.string "status", default: "", null: false
-    t.string "uuid", default: "", null: false
   end
 
   create_table "users", force: :cascade do |t|
