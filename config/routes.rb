933b476bf9250f5c62d4e791526a3c9b991de1b9
kitano <ktnydi@yahoo.co.jp>
2019-06-27 17:51:19 +0900

    [fix] reference error of partial and search form

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,9 @@ Rails.application.routes.draw do
     post '/like' => 'likes#create'
     delete '/unlike' => 'likes#destroy'
   end
-  post 'posts/:uuid' => 'posts#update'
   resources :likes, only: [:index]
   resources :users, only: [:index, :show] do
     resources :follows, only: [:create, :destroy]
-    get '/posts' => 'users#index'
     get '/follows' => 'follows#follow', as: "follows_list"
     get '/followers' => 'follows#follower', as: "followers_list"
   end
