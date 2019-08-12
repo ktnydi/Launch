13a46c1dc823a573e838ba95ed844fbb0374d171
kitano <kitano.prog@gmail.com>
2019-08-12 18:44:04 +0900

    Add primary_key to model/draft.rb

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,4 +1,6 @@
 class Draft < ApplicationRecord
+  self.primary_key = "article_token"
+  
   belongs_to :user, foreign_key: "user_token"
   validates :article_token, presence: true, uniqueness: true
   validates :title, length: { maximum: 50 }
