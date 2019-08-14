79c5952887c8f529007222ac8065598b42ebc79c
kitano <kitano.prog@gmail.com>
2019-08-14 10:42:33 +0900

    [modify] home page ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
 
   private
     def get_trend_articles
-      @trend_articles = Public.get_trend_articles(date(params[:period]))
+      @trend_articles = Public.get_trend_articles(date(params[:period])).limit(10)
     end
 end
