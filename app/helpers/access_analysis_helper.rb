6a393579e2fc1a424028a02f7aa13ef0de2d1734
kitano <kitano.prog@gmail.com>
2019-08-27 18:22:12 +0900

    [modify] how to getting analytics for production environment
    In postgresql, moving is different with sqlite3.

--- app/helpers/access_analysis_helper.rb
+++ app/helpers/access_analysis_helper.rb
@@ -1,23 +1,35 @@
 module AccessAnalysisHelper
 
-  def access_count_since(period)
-    AccessAnalysis.joins_public.since(period)
-      .where("publics.user_token = ?", current_user.id)
-      .size
-  end
-
   def access_count_all_day
     current_user.publics.pluck(:access_analyses_count).sum
   end
 
-  def many_access_articles
-    AccessAnalysis.many_access_articles_since(29.days.ago)
-      .where("publics.user_token = ?", current_user.id)
-      .limit(5)
+  def user_analyses_since(period)
+    article_tokens = current_user.publics.pluck(:article_token)
+    AccessAnalysis.where(article_token: article_tokens).where("created_at > ?", period)
   end
 
-  def most_access_article_sources
-    token = many_access_articles.first.article_token
-    AccessAnalysis.article_source_of(token).limit(5)
+  def access_since(period)
+    user_analyses_since(period).size
+  end
+
+  def popular_articles
+    article_pvs = user_analyses_since(30.days.ago).group(:article_token).order("count_article_token desc").limit(5).count(:article_token)
+    articles = article_pvs.map { |article_token, pv|
+      Public.find_by(article_token: article_token).attributes.merge("pv" => pv)
+    }
+  end
+
+  # return type is hash : { "id" => 123, "access_source" => "http://example.com", "pv" => 123, "rate" => 123.4 }
+  def most_popular_article_sources
+    return [] unless popular_articles.length > 0
+
+    article = popular_articles.first
+    access_analyses = AccessAnalysis.where(article_token: article["article_token"])
+    sources = access_analyses.select("access_source, count(access_source) as pv").group(:access_source).order("pv desc").limit(5)
+    sources.map do |source|
+      rate = (source.pv * 100.0 / access_analyses.size).round(1)
+      source.attributes.merge("rate" => rate)
+    end
   end
 end
