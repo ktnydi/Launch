342759b30a1a146c4906faa3da1882d05adbcb98
kitano <kitano.prog@gmail.com>
2019-09-08 15:02:38 +0900

    [Update] rails
    * version 6.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Launch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
