3479688b7b2c285a7bc920720ed4ddfe7bada3e0
kitano <kitano.prog@gmail.com>
2019-10-28 23:50:25 +0900

    Rename column to tags

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,7 +4,7 @@ class Entry < ApplicationRecord
   belongs_to :user, foreign_key: "user_token"
 
   validates :title, presence: true, length: { maximum: 50 }
-  validates :tag, length: { maximum: 50 }
+  validates :tags, length: { maximum: 50 }
   validates :content, length: { maximum: 10000 }
 
   has_secure_token :entry_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_131618) do
+ActiveRecord::Schema.define(version: 2019_10_28_065506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2019_10_25_131618) do
     t.string "token"
     t.integer "status", default: 0, null: false
     t.string "title", default: "", null: false
-    t.string "tag", default: ""
+    t.text "tags", default: ""
     t.text "content", default: ""
     t.string "user_token"
     t.datetime "created_at", precision: 6, null: false
