eb6e517b2c6374fb96fc92ddb91322ec0fa4992e
kitano <kitano.prog@gmail.com>
2019-11-12 23:34:02 +0900

    Modify likes_controller
    - Change parameter to entry_token.
    - Change render to return json.
    - Remove unneeded action.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,33 +1,25 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_public, except: [:index]
 
   def index
-    @likes = current_user.liked_article.page(params[:page]).per(20)
+    @likes = current_user.liked_entries.page(params[:page]).per(20)
   end
 
   def create
-    @like = current_user && current_user.likes.new(article_token: params[:article_token])
-    respond_to do |format|
-      if @like.save
-        format.js { render 'like' }
-        format.html
-      end
+    like = current_user.likes.new(entry_token: params[:entry_token])
+    if like.save
+      render json: like, status: :created
+    else
+      render json: like.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @like = current_user.likes.find_by(article_token: params[:article_token])
-    respond_to do |format|
-      if @like.destroy
-        format.js { render 'like' }
-        format.html
-      end
+    like = current_user.likes.find_by(entry_token: params[:entry_token])
+    if like.destroy
+      head :no_content
+    else
+      render like.errors, status: :unprocessable_entity
     end
   end
-
-  private
-    def set_public
-      @public = Public.find_by(article_token: params[:article_token])
-    end
 end
