315980847ce32b15f066bb4255dee57464de5cc3
kitano <kitano.prog@gmail.com>
2019-11-16 11:50:00 +0900

    Rename article column to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_083816) do
+ActiveRecord::Schema.define(version: 2019_11_16_001958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_083816) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_token", null: false
-    t.string "article_token", null: false
+    t.string "entry_token", null: false
     t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
